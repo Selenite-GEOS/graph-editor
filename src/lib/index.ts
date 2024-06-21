666022fdd5a2aab44d579880679f06fe812d91fb
Lyss <shaitan.lyssm@gmail.com>
2024-06-21 22:13:12 +0200

    Add exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
 export {setupEditor} from '$rete/editor'
 export {setupClasses} from '$rete/setup/appLaunch'
+export * from '$rete/node/MacroNode'
+export * from '$rete/node/XML/XmlNode'
+export * from '$rete'
