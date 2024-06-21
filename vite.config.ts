54f8cbc711c1b89b8b1e6f2bc5414748d502a1d0
Lyss <shaitan.lyssm@gmail.com>
2024-06-21 21:22:10 +0200

    change target

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import houdini from 'houdini/vite'
-import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   plugins: [houdini(), sveltekit()],
+  build: {
+    target: "esnext",
+  },
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
