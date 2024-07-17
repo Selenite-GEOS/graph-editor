d22ae8d1f6e325db5fd9cfd6dd490a9aadd5628f
Lyss <shaitan.lyssm@gmail.com>
2024-07-18 00:42:56 +0200

    Add example XML Generation

--- src/lib/graph-editor/examples/index.ts
+++ src/lib/graph-editor/examples/index.ts
@@ -1,2 +1,3 @@
 export {default as BasicDatatypes} from './Basic datatypes.json'
 export {default as BasicControlFlow} from './Basic controlflow.json'
+export {default as XmlGeneration} from './XML Generation.json'
