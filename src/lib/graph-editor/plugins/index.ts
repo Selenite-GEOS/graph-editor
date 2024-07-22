c73d710c757ab8259fd5e83b28dffe6e26955034
Lyss <shaitan.lyssm@gmail.com>
2024-07-22 22:10:52 +0200

    Export modal

--- src/lib/graph-editor/plugins/index.ts
+++ src/lib/graph-editor/plugins/index.ts
@@ -3,5 +3,6 @@ export * as History from './history';
 export * as TypedSockets from './typed-sockets';
 export * as ContextMenu from './context-menu';
 export * from './context-menu'
+export * from './modal'
 export * as ViewportAddons from './viewport-addons'
 export * as Notifications from './notifications'
