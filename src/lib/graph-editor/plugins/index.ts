d456f57896afc4a5dcd5c53fa486d183fa861bd8
Lyss <shaitan.lyssm@gmail.com>
2024-07-22 21:21:30 +0200

    Add export context menu component

--- src/lib/graph-editor/plugins/index.ts
+++ src/lib/graph-editor/plugins/index.ts
@@ -2,5 +2,6 @@ export * as Comment from './CommentPlugin';
 export * as History from './history';
 export * as TypedSockets from './typed-sockets';
 export * as ContextMenu from './context-menu';
+export * from './context-menu'
 export * as ViewportAddons from './viewport-addons'
 export * as Notifications from './notifications'
