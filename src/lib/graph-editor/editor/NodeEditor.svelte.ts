1db244c007f919bdd05ed95e150e7f312cdcf4cf
Lyss <shaitan.lyssm@gmail.com>
2024-08-12 11:47:57 +0200

    Revert changes

--- src/lib/graph-editor/editor/NodeEditor.svelte.ts
+++ src/lib/graph-editor/editor/NodeEditor.svelte.ts
@@ -45,7 +45,7 @@ export class NodeEditor extends BaseNodeEditor<Schemes> {
 		return this.#graphName;
 	}
 	set graphName(n) {
-		this.#graphName = n;
+		this.#graphName = n.trim() !== '' ? n.trim() : 'Untitled';
 		this.onChangeNameListeners.forEach((listener) => listener(n));
 	}
 	nameStore: Readable<string> = {
