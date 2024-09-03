bd0fcf08d8e819cb3ec350c24bd386da0532458b
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-03 21:59:13 +0200

    Make buttons primary

--- src/lib/graph-editor/render/svelte/presets/classic/components/Button.svelte
+++ src/lib/graph-editor/render/svelte/presets/classic/components/Button.svelte
@@ -2,10 +2,12 @@
 	import type { ButtonControl } from '../../../../../control/button/button.svelte';
 
 	export let data: ButtonControl;
+
+	const isPlay = data.label?.toLowerCase() === 'play';
 </script>
 
 <button
-	class="btn variant-filled"
+	class="btn btn-primary" class:m-2={isPlay}
 	on:dblclick|stopPropagation
 	on:pointerdown|stopPropagation={() => null}
 	on:click={data.onClick}
